f04a7b667ad93923a760a81259363bd46e5786f8
Stephan Schneider <stephanschneider34@hotmail.com>
2023-02-28 22:09:26 +0100

    Erweiterung der Klasse ShiftCalendar.java
    - Der Kalender kann mit frei gewähltem Anfangs- und Enddatum innerhalb eines Jahres erstellt werden

--- src/main/java/shiftplan/calendar/ShiftCalendar.java
+++ src/main/java/shiftplan/calendar/ShiftCalendar.java
@@ -5,14 +5,20 @@ import org.apache.logging.log4j.Logger;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.time.Month;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class ShiftCalendar {
 
     private static final Logger logger = LogManager.getLogger(ShiftCalendar.class);
+
+    private int calendarForYear;
+    record CalendarInfo(int calendarWeekIndex, LocalDate dateBoundary) {}
+
+    public ShiftCalendar() {}
+
+    public ShiftCalendar(int year) {calendarForYear = year;}
 
     LocalDate getFirstCalendarWeekStart(int year) {
         LocalDate firstDay = LocalDate.of(year,1,1); // 01. Januar des Jahres <year>
@@ -62,5 +68,142 @@ public class ShiftCalendar {
             ref.start = calendarWeek[calendarWeek.length -1].plusDays(1);
         });
         return calendarWeeks;
+    }
+
+    Map<Integer, LocalDate[]> createCalendar(LocalDate start, LocalDate end) {
+        LocalDate firstDayInCW = getFirstCalendarWeekStart(calendarForYear);
+
+        if (start.getMonth() == Month.JANUARY) {
+            // Die erste Kalenderwoche eines Jahres (KW 1) kann nach dem 01.01. des betreffenden Jahres beginnen.
+            // In diesen Fällen liegt der Beginn des Schichtplans, der normalerweise mit dem 01. des Monats angegeben
+            // wird, vor dem Beginn der ersten Kalenderwoche. Das Startdatum des Kalenders muss daher in diesem
+            // speziellen Fall am Jahresanfang auf das Datum des ersten Tages der KW 1 gesetzt werden, um einen
+            // korrekten Ablauf des Programms zu gewährleisten.
+            if (start.isBefore(firstDayInCW)) {
+                start = firstDayInCW;
+            }
+        }
+
+        if (end.getMonth() == Month.DECEMBER) {
+            // Erläuterungen über den Grund einer eventuellen Adjustierung des Enddatum im Dezember siehe Kommentar in
+            // <adjustEndDateForDecember>
+            end = adjustEndDateForDecember(end);
+        }
+
+        CalendarInfo startCw = getCalendarWeekOfDate(firstDayInCW, start, true);
+        CalendarInfo endCw = getCalendarWeekOfDate(firstDayInCW, end, false);
+
+        var ref = new Object() {
+            LocalDate start = startCw.dateBoundary();
+        };
+
+        int rangeEnd = getMaxCalendarWeek(calendarForYear);
+
+        Map<Integer, LocalDate[]> calendarWeeks = new HashMap<>();
+        IntStream.range(0, rangeEnd)
+                .takeWhile(cwIndex -> (cwIndex +1) <= endCw.calendarWeekIndex())
+                .forEach(cwIndex -> {
+            logger.trace("*** Kalenderwoche {} im Jahr {} ***", cwIndex +1, startCw.dateBoundary().getYear());
+
+            // Kalenderwoche liegt vor dem angeforderten Start des Kalenders / Schichtplans
+            if ((cwIndex +1) < startCw.calendarWeekIndex) return;
+
+            List<LocalDate> calendarWeekList = ref.start.datesUntil(ref.start.plusDays(7)).toList();
+            LocalDate[] calendarWeek = calendarWeekList.toArray(new LocalDate[] {});
+            calendarWeeks.put(cwIndex +1, calendarWeek);
+
+            logger.trace("CW {} von {} bis {}",
+                    cwIndex +1, calendarWeek[0], calendarWeek[calendarWeek.length -1]);
+            ref.start = calendarWeek[calendarWeek.length -1].plusDays(1);
+        });
+
+        return calendarWeeks;
+    }
+
+    CalendarInfo getCalendarWeekOfDate(final LocalDate firstDayInCalendar, final LocalDate date, boolean calendarStart) {
+        // Ermitteln der Kalenderwoche anhand eines Start- bzw. Enddatums. Die ermittelte Start- und Endkalenderwoche
+        // bestimmen die in shiftplan.xml definierte Zeitspanne des Kalenders (z.B. vom 01.04. - 30.09.)
+        boolean startDateIsWeekend =
+                date.getDayOfWeek() == DayOfWeek.SATURDAY || date.getDayOfWeek() == DayOfWeek.SUNDAY;
+
+        final var calendarInfoWrapper = new Object() {
+            CalendarInfo calendarInfo = null;
+        };
+        final var firstDayInCWWrapper = new Object() {
+            LocalDate firstDayInCW = firstDayInCalendar;
+        };
+
+        int rangeEnd = getMaxCalendarWeek(calendarForYear);
+
+        IntStream.range(0, rangeEnd)
+                .takeWhile(cwIndex -> calendarInfoWrapper.calendarInfo == null)
+                .forEach(cwIndex -> {
+            logger.debug("Prüfen von Kalenderwoche {}", cwIndex +1);
+
+            List<LocalDate> calendarWeekList =
+                            firstDayInCWWrapper.firstDayInCW.datesUntil(firstDayInCWWrapper.firstDayInCW.plusDays(7)).toList();
+            LocalDate[] calendarWeek = calendarWeekList.toArray(new LocalDate[] {});
+            LocalDate cwStart = calendarWeek[0]; // Montag der jeweiligen KW
+            logger.debug("cwStart in KW {}: {}", cwIndex +1, cwStart);
+            LocalDate cwEnd = calendarWeek[calendarWeek.length - 1]; // Sonntag der jeweiligen KW
+            logger.debug("cwEnd in KW {}: {}", cwIndex +1, cwEnd);
+
+            if ((date.isEqual(cwStart) || date.isAfter(cwStart))
+                    && (date.isEqual(cwEnd) || date.isBefore(cwEnd))) {
+                if (startDateIsWeekend && calendarStart) {
+                    // Wenn das Startdatum <date> auf ein Wochenende fällt, beginnt der Kalender mit der
+                    // darauffolgenden Woche, da in der aktuellen Kalenderwoche kein Arbeitstag liegt
+                    calendarInfoWrapper.calendarInfo = new CalendarInfo(cwIndex +2, cwEnd.plusDays(1));
+                } else {
+                    calendarInfoWrapper.calendarInfo = new CalendarInfo(cwIndex + 1, cwStart);
+                }
+                logger.debug("Kalender {} in KW {} mit Wochenstart-Datum {}",
+                        calendarStart ? "startet" : "endet",
+                        calendarInfoWrapper.calendarInfo.calendarWeekIndex,
+                        calendarInfoWrapper.calendarInfo.dateBoundary);
+            }
+            firstDayInCWWrapper.firstDayInCW = cwEnd.plusDays(1);
+        });
+        return calendarInfoWrapper.calendarInfo;
+    }
+
+    LocalDate adjustEndDateForDecember(LocalDate calendarEnd) {
+        // Der Algorithmus für das Bestimmen der letzten Kalenderwoche des Wochenkalenders in <getCalendarWeekOfDate>
+        // funktioniert nicht korrekt, wenn A): Das Enddatum im Dezember liegt und B): Der 31.01., der in diesem Fall
+        // in shiftplan.xml normalerweise als das Enddatum des Kalenders angegeben wird, in der KW 1 des Folgejahres
+        // liegt.
+        // In dieser Konstellation muss das Enddatum auf das letzte Datum korrigiert werden, das in die letzte
+        // KW (52 oder 53) fällt.
+
+        // Aufruf dieser Methode nur für die Bestimmung des Kalenderendes im Monat Dezember zulässig!
+        assert calendarEnd.isEqual(LocalDate.of(calendarForYear,12,31));
+
+        var calendarEndWrapper = new Object() {
+            LocalDate calEnd = calendarEnd;
+        };
+        LocalDate cwStartInNextYear = getFirstCalendarWeekStart(calendarForYear +1); // KW 1 im Folgejahr
+        List<LocalDate> cw1InNextYear = cwStartInNextYear.datesUntil(cwStartInNextYear.plusDays(7)).toList();
+        // Prüfen, ob der 31.12. in der ersten KW des Folgejahres liegt.
+        LocalDate thirtyOneOfDec = cw1InNextYear
+                .stream()
+                .filter(date -> date.isEqual(calendarEndWrapper.calEnd))
+                .findFirst()
+                .orElse(null);
+        if (thirtyOneOfDec != null) {
+            // Der 31.12. fällt in die KW 1 des Folgejahres. Das Enddatum des Kalenders muss auf den letzten Sonntag
+            // des Jahres gesetzt werden.
+           if (calendarEnd.getDayOfWeek() == DayOfWeek.WEDNESDAY) {
+               calendarEndWrapper.calEnd = calendarEnd.minusDays(3);
+           } else if (calendarEnd.getDayOfWeek() == DayOfWeek.TUESDAY) {
+               calendarEndWrapper.calEnd  = calendarEnd.minusDays(2);
+           } else if (calendarEnd.getDayOfWeek() == DayOfWeek.MONDAY) {
+               calendarEndWrapper.calEnd  = calendarEnd.minusDays(1);
+           } else {
+               throw new IllegalArgumentException(
+                       "Das Enddatum des Kalenders muss immer der letzte Tag eines Monats sein");
+           }
+        }
+        logger.info("Enddatum im Dezember (per shiftplan.xml oder adjustiert): {}", calendarEndWrapper.calEnd);
+        return calendarEndWrapper.calEnd;
     }
 }
--- src/test/java/shiftplan/calendar/ShiftCalendarTest.java
+++ src/test/java/shiftplan/calendar/ShiftCalendarTest.java
@@ -9,7 +9,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -55,5 +57,113 @@ class ShiftCalendarTest {
         logger.debug("Erster Tag der KW 1: {}", startOfFirstWeek);
 
         List<LocalDate[]> calendar = shiftCalendar.createCalendar(year, startOfFirstWeek);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getInputForCreateCalendarWeeksWithStartEndDate")
+    void createCalendarWeeksWithStartEndDate(int year, LocalDate start, LocalDate end) {
+        ShiftCalendar shiftCalendar = new ShiftCalendar(year);
+        Map<Integer, LocalDate[]> calendarWeeks = shiftCalendar.createCalendar(start, end);
+        assertFalse(calendarWeeks.isEmpty());
+
+        for (Integer key : calendarWeeks.keySet()) {
+            logger.debug("Kalenderwoche {} / Beginn {}, Ende {}",
+                    key,
+                    calendarWeeks.get(key)[0],
+                    calendarWeeks.get(key)[6]);
+        }
+    }
+
+    private static Stream<Arguments> getInputForCreateCalendarWeeksWithStartEndDate() {
+        return Stream.of(
+                Arguments.of(2023, LocalDate.of(2023,3,1), LocalDate.of(2023,9,30)),
+                Arguments.of(2023, LocalDate.of(2023,1,2), LocalDate.of(2023,12,31)),
+                Arguments.of(2023, LocalDate.of(2023,4,1), LocalDate.of(2023,10,31)),
+                Arguments.of(2024, LocalDate.of(2024,3,1), LocalDate.of(2024,12,31)),
+                Arguments.of(2025, LocalDate.of(2025,1,1), LocalDate.of(2025,12,31))
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getStartDates")
+    void getCalendarStart(int year, LocalDate startDate) {
+        ShiftCalendar shiftCalendar = new ShiftCalendar(year);
+
+        LocalDate firstDayInCW = shiftCalendar.getFirstCalendarWeekStart(year);
+        logger.debug("firstDayInCW: {}", firstDayInCW);
+
+        if (startDate.getMonth() == Month.JANUARY) {
+            if (startDate.isBefore(firstDayInCW)) {
+                startDate = firstDayInCW;
+            }
+        }
+
+        ShiftCalendar.CalendarInfo calendarStart = shiftCalendar.getCalendarWeekOfDate(
+                firstDayInCW, startDate, true);
+        assertNotNull(calendarStart);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getEndDates")
+    void getCalendarEnd(int year, LocalDate endDate) {
+        ShiftCalendar shiftCalendar = new ShiftCalendar(year);
+
+        if (endDate.getMonth() == Month.DECEMBER) {
+            endDate = shiftCalendar.adjustEndDateForDecember(endDate);
+        }
+
+        LocalDate firstDayInCW = shiftCalendar.getFirstCalendarWeekStart(year);
+        logger.debug("firstDayInCW: {}", firstDayInCW);
+
+        ShiftCalendar.CalendarInfo calendarEnd = shiftCalendar.getCalendarWeekOfDate(
+                firstDayInCW, endDate, false);
+        assertNotNull(calendarEnd);
+    }
+
+    private static Stream<Arguments> getStartDates() {
+        return Stream.of(
+                Arguments.of(2018, LocalDate.of(2018,1,1)),
+                Arguments.of(2019, LocalDate.of(2019,1,1)),
+                Arguments.of(2020, LocalDate.of(2020,1,1)),
+                Arguments.of(2021, LocalDate.of(2021,1,1)),
+                Arguments.of(2022, LocalDate.of(2022,1,1)),
+                Arguments.of(2023, LocalDate.of(2023, 1,1)),
+                Arguments.of(2024, LocalDate.of(2024,1,1)),
+                Arguments.of(2025, LocalDate.of(2025,1,1)),
+                Arguments.of(2026, LocalDate.of(2026,1,1)),
+                Arguments.of(2027, LocalDate.of(2027,1,1)),
+                Arguments.of(2028, LocalDate.of(2028,1,1)),
+                Arguments.of(2023, LocalDate.of(2023, 4,1)),
+                Arguments.of(2023,LocalDate.of(2023,6,1)),
+                Arguments.of(2023,LocalDate.of(2023,10,1)),
+                Arguments.of(2023, LocalDate.of(2023, 9,1))
+        );
+    }
+
+    private static Stream<Arguments> getEndDates() {
+        return Stream.of(
+                Arguments.of(2018, LocalDate.of(2018,12,31)),
+                Arguments.of(2019, LocalDate.of(2019,12,31)),
+                Arguments.of(2020, LocalDate.of(2020,12,31)),
+                Arguments.of(2021, LocalDate.of(2021,12,31)),
+                Arguments.of(2022, LocalDate.of(2022,12,31)),
+                Arguments.of(2023, LocalDate.of(2023, 12,31)),
+                Arguments.of(2024, LocalDate.of(2024,12,31)),
+                Arguments.of(2025, LocalDate.of(2025,12,31)),
+                Arguments.of(2026, LocalDate.of(2026,12,31)),
+                Arguments.of(2027, LocalDate.of(2027,12,31)),
+                Arguments.of(2028, LocalDate.of(2028,12,31)),
+                Arguments.of(2023, LocalDate.of(2023, 4,30)),
+                Arguments.of(2023,LocalDate.of(2023,7,31)),
+                Arguments.of(2023,LocalDate.of(2023,10,31))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getEndDates")
+    void calendarEndTest(int year, LocalDate endDate) {
+        ShiftCalendar shiftCalendar = new ShiftCalendar(year);
+        LocalDate adjustedEndDateForDecember = shiftCalendar.adjustEndDateForDecember(endDate);
     }
 }
